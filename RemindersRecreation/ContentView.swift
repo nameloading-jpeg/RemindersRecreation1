1fda8adcc37bf90115ee2d2ffde7090bd4f9adb9
nameloading-jpeg <mdsun@unc.edu>
2026-02-26 14:18:06 -0500

    Initial Commit

---
+++ RemindersRecreation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RemindersRecreation
+//
+//  Created by Mia Sundman on 2/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RemindersRecreation/RemindersRecreationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersRecreationApp.swift
+//  RemindersRecreation
+//
+//  Created by Mia Sundman on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct RemindersRecreationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
